a247fa26d02de71ddf8f64a2e0fc84ca2cef462b
Aman Yerpude <Aman@192.168.0.148>
2024-05-26 20:15:15 +0530

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { Vue2BasicComponentLibrarySample } from '@/entry.esm';
+import { FccButton } from "@/entry.esm";
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    FccButton
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <FccButton />
+  </div>
+</template>
